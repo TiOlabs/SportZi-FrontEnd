f226c604243ea58ca96d0adf3956989b6ecb11e1
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-01-27 11:47:06 +0530

    coach cards with background

--- src/pages/home/coachCardSection.tsx
+++ src/pages/home/coachCardSection.tsx
@@ -9,87 +9,108 @@ const CoachCardSection = () => {
 
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "100%",
-          height: "auto",
-          justifyContent: "center",
-          alignItems: "center",
-          background: "rgba(27, 93, 183, 0.07)",
-        }}
-      >
-        <div
-          style={{ width: "100%", alignItems: "center", textAlign: "center" }}
+      <Row style={{ width: "100%" }}>
+        <Col
+          xs={{ span: 24 }}
+          sm={{ span: 24 }}
+          md={{ span: 24 }}
+          lg={{ span: 24 }}
+          xl={{ span: 24 }}
         >
-          <h1 style={{ color: " #0E458E" }}>Our Best Coaches</h1>
-        </div>
-        <button
-          style={{
-            color: "#1B5DB7",
-            background: "none",
-            border: "none",
-            fontFamily: "Kanit",
-            fontSize: "18px",
-            position: "absolute",
-            zIndex: "999",
-          }}
-        >
-          See More
-        </button>
-
-        <div
-          style={{
-            marginBottom: "5%",
-            display: "flex",
-            width: "100%",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Row
-            style={{ width: "100%", display: "flex", justifyContent: "center" }}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              height: "auto",
+              justifyContent: "center",
+              alignItems: "center",
+              background: "rgba(27, 93, 183, 0.07)",
+            }}
           >
-            <Col
-              xs={{ span: 24 }}
-              sm={{ span: 12 }}
-              md={{ span: 8 }}
-              lg={{ span: 5 }}
-              xl={{ span: 5 }}
+            <div
+              style={{
+                width: "100%",
+                alignItems: "center",
+                textAlign: "center",
+              }}
             >
-              <CoachCard />
-            </Col>
-            <Col
-              xs={{ span: 24 }}
-              sm={{ span: 12 }}
-              md={{ span: 8 }}
-              lg={{ span: 5 }}
-              xl={{ span: 5 }}
+              <h1 style={{ color: " #0E458E" }}>Our Best Coaches</h1>
+            </div>
+
+            <div
+              style={{
+                marginBottom: "5%",
+                display: "flex",
+                width: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
             >
-              <CoachCard />
-            </Col>
-            <Col
-              xs={{ span: 24 }}
-              sm={{ span: 12 }}
-              md={{ span: 8 }}
-              lg={{ span: 5 }}
-              xl={{ span: 5 }}
-            >
-              <CoachCard />
-            </Col>
-            <Col
-              xs={{ span: 24 }}
-              sm={{ span: 12 }}
-              md={{ span: 8 }}
-              lg={{ span: 5 }}
-              xl={{ span: 5 }}
-            >
-              <CoachCard />
-            </Col>
-          </Row>
-        </div>
-      </div>
+              <button
+                style={{
+                  color: "#1B5DB7",
+                  background: "none",
+                  border: "none",
+                  fontFamily: "Kanit",
+                  fontSize: "18px",
+                  position: "absolute",
+                  zIndex: "999",
+                  top: "90px",
+                  right: "100px",
+                }}
+              >
+                See More
+              </button>
+              <Row
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <Col
+                  xs={{ span: 24 }}
+                  sm={{ span: 12 }}
+                  md={{ span: 8 }}
+                  lg={{ span: 5 }}
+                  xl={{ span: 5 }}
+                >
+                  <CoachCard />
+                </Col>
+                <Col
+                  xs={{ span: 24 }}
+                  sm={{ span: 12 }}
+                  md={{ span: 8 }}
+                  lg={{ span: 5 }}
+                  xl={{ span: 5 }}
+                >
+                  <CoachCard />
+                </Col>
+                <Col
+                  xs={{ span: 24 }}
+                  sm={{ span: 12 }}
+                  md={{ span: 8 }}
+                  lg={{ span: 5 }}
+                  xl={{ span: 5 }}
+                >
+                  <CoachCard />
+                </Col>
+                <Col
+                  xs={{ span: 24 }}
+                  sm={{ span: 12 }}
+                  md={{ span: 8 }}
+                  lg={{ span: 5 }}
+                  xl={{ span: 5 }}
+                >
+                  {" "}
+                  <CoachCard />
+                </Col>
+              </Row>
+            </div>
+          </div>
+        </Col>
+      </Row>
     </>
   );
 };
