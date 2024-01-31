cacfa74c0e9f82c658a83cee66e0bdaf1e97511d
Ranasinghe H.B.C.B <141268625+Bhathiya2001@users.noreply.github.com>
2024-02-01 02:18:40 +0530

    ArcadeRatingCard
    1st creat need to dev

--- src/pages/home/arcadeRatingCardSection.tsx
+++ src/pages/home/arcadeRatingCardSection.tsx
@@ -1,8 +1,32 @@
 import { Col, Row } from 'antd';
+import * as styles from './home.module.css';
+import { ColorFactory } from 'antd/es/color-picker/color';
+import ButtonGroup from 'antd/es/button/button-group';
+import ArcadeRatingCard from '../../components/arcadeRatingCard';
+import { Route } from 'react-router-dom';
+import Column from 'antd/es/table/Column';
 const ArcadeRatingCardsSection = () => {
     return ( 
-        <Row>
-            <h1>ArcadeRatingCardsSection</h1>
+        <Row style={{justifyContent:"center", display:"flex", alignItems:"center", flexDirection:"column"}}>
+          
+                <h1 style={{color:"#0E458E" ,fontSize:"32px",fontStyle:"normal",fontWeight:"600",lineHeight:"normal", justifyContent:"center", display:"flex", alignItems:"center"}}>Arcade ratings</h1>
+                <Row>
+                    <Col xs={1} md={2} lg={2} xl={2}></Col>
+                    <Col xs={23} md={22} lg={22} xl={10}>
+                    <ArcadeRatingCard/>
+                    </Col>
+                    <Col xs={1} md={12} lg={12} xl={2}></Col>
+                    <Col xs={23} md={12} lg={12} xl={10}>
+                    <ArcadeRatingCard/>
+                    </Col>
+                    <Col xs={1} md={2} lg={2} xl={8}></Col>
+                    <Col xs={23} md={22} lg={22} xl={16}>
+                    <ArcadeRatingCard/>
+                    </Col>
+                </Row>
+                
+          
+
         </Row>
      );
 }
