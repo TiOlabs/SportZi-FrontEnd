f898b783caadc69b663b52397ac91eee19b52b2f
Ranasinghe H.B.C.B <141268625+Bhathiya2001@users.noreply.github.com>
2024-02-12 03:32:06 +0530

    Arcade Rating card upgrade 01
    responsive position change in this

--- src/pages/home/arcadeRatingCardSection.tsx
+++ src/pages/home/arcadeRatingCardSection.tsx
@@ -11,16 +11,23 @@ const ArcadeRatingCardsSection = () => {
           
                 <h1 style={{color:"#0E458E" ,fontSize:"32px",fontStyle:"normal",fontWeight:"600",lineHeight:"normal", justifyContent:"center", display:"flex", alignItems:"center"}}>Arcade ratings</h1>
                 <Row>
-                    <Col xs={1} md={2} lg={2} xl={2}></Col>
-                    <Col xs={23} md={22} lg={22} xl={10}>
+                    <Col xs={1} md={0} lg={0} xl={1}></Col>
+                    <Col xs={23} md={12} lg={8} xl={8}>
                     <ArcadeRatingCard/>
                     </Col>
-                    <Col xs={1} md={12} lg={12} xl={2}></Col>
-                    <Col xs={23} md={12} lg={12} xl={10}>
+                    <Col xs={1} md={0} lg={0} xl={0}></Col>
+                    <Col xs={23} md={12} lg={8} xl={7}>
                     <ArcadeRatingCard/>
                     </Col>
-                    <Col xs={1} md={2} lg={2} xl={8}></Col>
-                    <Col xs={23} md={22} lg={22} xl={16}>
+                    <Col xs={1} md={6} lg={0} xl={0}></Col>
+                    <Col xs={23} md={12} lg={8} xl={8}>
+                    <ArcadeRatingCard/>
+                    </Col>
+                    <Col xs={0} md={0} lg={0} xl={4}></Col>
+                    <Col xs={0} md={0} lg={0} xl={8}>
+                    <ArcadeRatingCard/>
+                    </Col>
+                    <Col xs={0} md={0} lg={0} xl={8}>
                     <ArcadeRatingCard/>
                     </Col>
                 </Row>
