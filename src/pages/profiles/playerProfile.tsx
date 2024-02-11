272fd2c8373a0abb9e26747a1b9d1f3329333771
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-02-11 18:24:39 +0530

    first section

--- src/pages/profiles/playerProfile.tsx
+++ src/pages/profiles/playerProfile.tsx
@@ -1,11 +1,17 @@
-import { Col, Row } from "antd";
+import { Col, Flex, Row } from "antd";
 import { url } from "inspector";
 import backgroundImg from "../../assents/background2.png";
 import profileBackground from "../../assents/profileBackground.png";
 import profilePic from "../../assents/Home-pic-2.png.png";
+import { StarOutlined, StarFilled, StarTwoTone } from "@ant-design/icons";
+import { List } from "antd";
 const PlayerProfile = () => {
   return (
     <>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap')
+      </style>
       <Row>
         <Col
           xs={24}
@@ -45,8 +51,249 @@ const PlayerProfile = () => {
             backgroundSize: "cover",
             backgroundPosition: "center",
             height: "1000px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
-        ></Col>
+        >
+          <div
+            style={{
+              width: "80%",
+              height: "800px",
+
+              display: "flex",
+              justifyContent: "flex-start",
+              flexDirection: "column",
+            }}
+          >
+            <div>
+              <h1
+                style={{
+                  color: "#000",
+
+                  fontSize: "32px",
+                  fontStyle: "normal",
+                  fontWeight: "500",
+                  fontFamily: "kanit",
+                  lineHeight: "normal",
+                  marginBottom: "0px",
+                }}
+              >
+                Sandun Malage
+              </h1>
+              <p
+                style={{
+                  margin: "0px",
+                  color: "#000",
+                  fontFamily: "kanit",
+
+                  fontSize: "18px",
+                  fontStyle: "normal",
+                  fontWeight: "400",
+                  lineHeight: "normal",
+                }}
+              >
+                First class rugby coach
+              </p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                width: "100%",
+              }}
+            >
+              <Row>
+                <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                  <p
+                    style={{
+                      color: "#0E458E",
+                      fontFamily: "kanit",
+                      fontSize: "39px",
+                      fontStyle: "normal",
+                      fontWeight: "500",
+                      lineHeight: "normal",
+                      margin: "0px",
+                    }}
+                  >
+                    5.0
+                  </p>
+                </Col>
+
+                <Col
+                  xs={24}
+                  sm={24}
+                  md={12}
+                  lg={12}
+                  xl={12}
+                  style={{
+                    display: "flex",
+
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <div
+                    style={{
+                      height: "auto",
+                      position: "relative",
+                      width: "max-content",
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        width: "100%",
+                      }}
+                    >
+                      <StarFilled style={{ color: "#0E458E" }} />
+                      <StarFilled style={{ color: "#0E458E" }} />
+                      <StarFilled style={{ color: "#0E458E" }} />
+                      <StarTwoTone twoToneColor="#0E458E" />
+                      <StarTwoTone twoToneColor="#0E458E" />
+                    </div>
+                    <p
+                      style={{
+                        color: "#000",
+                        opacity: "0.64",
+                        fontFamily: "kanit",
+                        fontSize: "10px",
+                        fontStyle: "normal",
+                        fontWeight: "500",
+                        lineHeight: "normal",
+                        margin: "0px",
+                      }}
+                    >
+                      120 Feedbacks
+                    </p>
+                  </div>{" "}
+                </Col>
+              </Row>
+            </div>
+            <p
+              style={{
+                color: "#000",
+                fontFamily: "kanit",
+                opacity: ".96",
+                fontSize: "18px",
+                fontStyle: "normal",
+                fontWeight: "300",
+                lineHeight: "normal",
+                marginTop: "0px",
+              }}
+            >
+              I am a former elite rugby league player who would love to
+              encourage and mentor younger athletes to work towards their goals
+              and aspirations as well as to share my knowledge and give back to
+              the game that’s given me so much. My main position in rugby league
+              was halfback and I had the honour of representing QLD in the State
+              Of Origin
+            </p>
+            <p
+              style={{
+                color: "#000",
+                fontFamily: "kanit",
+
+                fontSize: "24px",
+                fontStyle: "normal",
+                fontWeight: "400",
+                lineHeight: "normal",
+                marginTop: "0px",
+              }}
+            >
+              Sports
+            </p>
+            <List
+              style={{
+                padding: "0px",
+                fontWeight: "200",
+                color: "#000",
+                fontFamily: "kanit",
+
+                fontSize: "20px",
+              }}
+              itemLayout="horizontal"
+              dataSource={["T20", "T20", "T20"]}
+              renderItem={(item) => (
+                <List.Item
+                  style={{
+                    position: "relative",
+                    top: "-20px",
+                    listStyle: "none",
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    alignItems: "center",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {" "}
+                    <span style={{ fontSize: "30px" }}>&#8226;</span>
+                    {item}
+                  </div>
+                </List.Item>
+              )}
+            />
+
+            <p
+              style={{
+                color: "#000",
+                fontFamily: "kanit",
+
+                fontSize: "24px",
+                fontStyle: "normal",
+                fontWeight: "400",
+                lineHeight: "normal",
+                marginTop: "0px",
+              }}
+            >
+              Achivements
+            </p>
+            <List
+              style={{
+                padding: "0px",
+              }}
+              itemLayout="horizontal"
+              dataSource={["T20", "T20", "T20"]}
+              renderItem={(item) => (
+                <List.Item
+                  style={{
+                    position: "relative",
+                    top: "-20px",
+                    listStyle: "none",
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    alignItems: "center",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {" "}
+                    <span style={{ fontSize: "30px" }}>&#8226;</span>
+                    {item}
+                  </div>
+                </List.Item>
+              )}
+            />
+          </div>
+        </Col>
       </Row>
       <div
         style={{
