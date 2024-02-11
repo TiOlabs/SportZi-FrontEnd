0e6c252e257c72b7f506145c6a804821fdd01ffa
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-02-11 19:01:05 +0530

    Discount Card Form Connect with backend

--- src/pages/home/discountCardSection.tsx
+++ src/pages/home/discountCardSection.tsx
@@ -1,36 +1,51 @@
 import { Col, Row } from "antd";
 import DiscountCard from "../../components/discountCard";
+import { useEffect, useState } from "react";
 const DiscoutCardsSection = () => {
+  const [discounts, setDiscounts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("http://localhost:8000/api/getdiscoutcardvalues");
+      const data = await res.json();
+      setDiscounts(data);
+
+      console.log(data);
+    }
+    fetchData();
+  }, []);
+  console.log(discounts);
   return (
-    
-    <Row style={{ display: "flex", flexDirection: "column",justifyContent:"center" }}>
+    <Row
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
       <h1
         style={{ color: "#24338a", display: "flex", justifyContent: "center" }}
       >
         Special discounts for you
       </h1>
 
-      
-
-      <Row style={{ display: "flex",justifyContent:"center" }}>
-        <Col lg={{span:8}}  md={{span:12}}  sm={{span:24}}>
-          <DiscountCard />
-        </Col>
-        <Col lg={{span:8}}  md={{span:12}}  sm={{span:24}}>
-          <DiscountCard />
-        </Col>
-        <Col lg={{span:8}}  md={{span:12}}  sm={{span:24}}>
-          <DiscountCard />
-        </Col>
-        <Col lg={{span:8}}  md={{span:12}}  sm={{span:24}}>
-          <DiscountCard />
-        </Col>
-        <Col lg={{span:8}}  md={{span:12}}  sm={{span:24}}>
-          <DiscountCard />
-        </Col>
-        <Col lg={{span:8}}  md={{span:12}}  sm={{span:24}}>
-          <DiscountCard />
-        </Col>
+      <Row
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          height: "415px",
+          overflow: "auto",
+        }}
+      >
+        {discounts.map((discount: any) => (
+          <Col lg={{ span: 8 }} md={{ span: 12 }} sm={{ span: 24 }}>
+            <DiscountCard
+              discount_percentage={discount.discount_percentage}
+              description={discount.description}
+              discount_image={discount.discount_image}
+            />
+          </Col>
+        ))}
       </Row>
     </Row>
   );
