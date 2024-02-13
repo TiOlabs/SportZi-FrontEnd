cd0ae20416f7bf878fb346fa859d9673563f20b1
Ranasinghe H.B.C.B <141268625+Bhathiya2001@users.noreply.github.com>
2024-02-14 01:26:18 +0530

    1st debug in NavBar
    need to solution for the contact us

--- src/pages/arcades/arcades.tsx
+++ src/pages/arcades/arcades.tsx
@@ -1,12 +1,16 @@
+import Navbar from "../../components/navbar";
 import ArcadeCardSection from "./arcadeCardSection";
 import HeroSection from "./heroSection";
 
 const Arcades = () => {
     return ( 
-        <div style={{display:"flex", flexDirection:"column",justifyContent:"center",alignItems:"center",lineHeight:"3"}}>
-             <HeroSection/>
-            <ArcadeCardSection/>
-       </div>
+        <><Navbar />
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", lineHeight: "3" }}>
+          
+              <HeroSection />
+              <ArcadeCardSection />
+         </div>
+         </>
      );
 }
  
