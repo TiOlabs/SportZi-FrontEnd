0cb060642b2e093c72b20cfac1d9c4e79f5a3059
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-02-16 00:34:14 +0530

    coach profile after popups

--- src/components/CoachRequestList.tsx
+++ src/components/CoachRequestList.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Grid, Row } from "antd";
+import { Button, Col, Grid, Modal, Row, Typography } from "antd";
 import { useState } from "react";
 import profilePic from "../assents/pro.png";
 
@@ -7,6 +7,8 @@ const CoachReqestList = () => {
   const { lg, md, sm, xs } = useBreakpoint();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpenWarning, setIsModalOpenWarning] = useState(false);
+  const [isModalOpenAccept, setIsModalOpenAccept] = useState(false);
 
   const showModal = () => {
     setIsModalOpen(true);
@@ -19,10 +21,35 @@ const CoachReqestList = () => {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+
+  const showModalWarning = () => {
+    setIsModalOpenWarning(true);
+  };
+
+  const handleOkWarning = () => {
+    // Close the first modal as well
+  };
+
+  const handleCancelWarning = () => {
+    setIsModalOpenWarning(false);
+    setIsModalOpen(false);
+  };
+
+  const showModalAccept = () => {
+    setIsModalOpenAccept(true);
+  };
+
+  const handleOkAccept = () => {
+    setIsModalOpenAccept(false);
+  };
+
+  const handleCancelAccept = () => {
+    setIsModalOpenAccept(false);
+    setIsModalOpen(false);
+  };
   return (
     <>
       <Row
-        onClick={showModal}
         style={{
           padding: "10px",
           width: "90%",
@@ -34,7 +61,7 @@ const CoachReqestList = () => {
           alignItems: "center",
         }}
       >
-        <Col xs={8} sm={8} md={8} lg={6} xl={6}>
+        <Col xs={8} sm={8} md={8} lg={6} xl={6} onClick={showModal}>
           <Row style={{ width: "100%" }}>
             <Col xs={24} sm={24} md={12} lg={12} xl={12}>
               <div
@@ -70,6 +97,7 @@ const CoachReqestList = () => {
           </Row>
         </Col>
         <Col
+          onClick={showModal}
           style={{
             color: "#000",
             fontFamily: "kanit",
@@ -111,6 +139,7 @@ const CoachReqestList = () => {
                 fontFamily: "kanit",
                 color: "#5587CC",
               }}
+              onClick={showModalWarning}
             >
               Reject Meeting
             </Button>
@@ -141,12 +170,237 @@ const CoachReqestList = () => {
                 fontFamily: "kanit",
               }}
               type="primary"
+              onClick={showModalAccept}
             >
               Accept Meeting
             </Button>
           </Col>
         )}
       </Row>
+
+      <Modal
+        title="Discription of Meeting"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[
+          <Button
+            style={{
+              backgroundColor: "#fff",
+              color: "#0E458E",
+              border: "1px solid #0E458E",
+              fontFamily: "kanit",
+            }}
+            key="back"
+            onClick={showModalWarning}
+          >
+            Reject Meeting
+          </Button>,
+          <Button
+            style={{
+              backgroundColor: "#0E458E",
+
+              fontFamily: "kanit",
+            }}
+            key="submit"
+            type="primary"
+            onClick={showModalAccept}
+          >
+            Accept Meeting
+          </Button>,
+        ]}
+      >
+        <Row
+          style={{
+            padding: "10px",
+            width: "90%",
+            height: "auto",
+            display: "flex",
+            justifyContent: "center",
+            border: "1px solid #EFF4FA",
+            alignItems: "center",
+          }}
+        >
+          <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Row style={{ width: "100%" }}>
+              <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                <div
+                  style={{
+                    backgroundColor: "#000",
+                    width: "90px",
+                    height: "81px",
+                    borderRadius: "50%",
+                    marginRight: "10px",
+                    backgroundImage: `url(${profilePic})`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </Col>
+              <Col
+                style={{
+                  color: "#000",
+                  fontFamily: "kanit",
+                  fontWeight: "300",
+                  fontSize: md ? "18px" : "16px",
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                }}
+                xs={24}
+                sm={24}
+                md={12}
+                lg={12}
+                xl={12}
+              >
+                kanishka jj
+              </Col>
+            </Row>
+          </Col>
+          <Col
+            style={{
+              color: "#000",
+              fontFamily: "kanit",
+              fontWeight: "300",
+              fontSize: md ? "18px" : "16px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            xl={12}
+          >
+            Date
+          </Col>
+        </Row>
+
+        <Row
+          style={{
+            padding: "10px",
+            width: "90%",
+            height: "auto",
+            display: "flex",
+            justifyContent: "center",
+
+            border: "1px solid #EFF4FA",
+            alignItems: "center",
+          }}
+        >
+          <Col
+            style={{
+              marginTop: "10px",
+              color: "#000",
+              fontFamily: "kanit",
+              fontWeight: "300",
+              fontSize: md ? "18px" : "16px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            xl={12}
+          >
+            Time
+          </Col>
+
+          <Col
+            style={{
+              marginTop: "10px",
+              color: "#000",
+              fontFamily: "kanit",
+              fontWeight: "300",
+              fontSize: md ? "18px" : "16px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            xl={12}
+          >
+            Venue
+          </Col>
+        </Row>
+      </Modal>
+
+      <Modal
+        title="Warning"
+        open={isModalOpenWarning}
+        onOk={handleOkWarning}
+        onCancel={handleCancelWarning}
+        footer={[
+          <Button
+            style={{
+              backgroundColor: "#fff",
+              color: "#0E458E",
+              border: "1px solid #0E458E",
+              fontFamily: "kanit",
+            }}
+            key="back"
+            onClick={handleCancelWarning}
+          >
+            Cancel
+          </Button>,
+          <Button
+            style={{
+              backgroundColor: "#0E458E",
+
+              fontFamily: "kanit",
+            }}
+            key="submit"
+            type="primary"
+            onClick={handleCancelWarning}
+          >
+            Reject Meeting
+          </Button>,
+        ]}
+      >
+        <Typography>
+          Are you shure reject the meeting in this student
+        </Typography>
+      </Modal>
+
+      <Modal
+        title="Accept Meeting"
+        open={isModalOpenAccept}
+        onOk={handleOkAccept}
+        onCancel={handleCancelAccept}
+        footer={[
+          <Button
+            style={{
+              backgroundColor: "#fff",
+              color: "#0E458E",
+              border: "1px solid #0E458E",
+              fontFamily: "kanit",
+            }}
+            key="back"
+            onClick={handleCancelAccept}
+          >
+            Cancel
+          </Button>,
+          <Button
+            style={{
+              backgroundColor: "#0E458E",
+
+              fontFamily: "kanit",
+            }}
+            key="submit"
+            type="primary"
+            onClick={handleCancelAccept}
+          >
+            Accept Meeting
+          </Button>,
+        ]}
+      >
+        <Typography>Are you shure accept meeting</Typography>
+      </Modal>
     </>
   );
 };
