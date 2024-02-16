4fb95383129b153ccbf659a73b897491aecfea0d
Thisara-Senarathna <thisarasenarathna0824@gmail.com>
2024-02-16 19:41:05 +0530

    setup signup

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,10 +4,12 @@ import Coaches from "../pages/coaches/coaches";
 import Arcades from "../pages/arcades/arcades";
 import About from "../pages/about/about";
 import Login from "../pages/login/login";
-import Signup from "../pages/signup/signup";
 import Profiles from "../pages/profiles/profiles";
 import BookingForm from "../pages/bookingForm/bookingForm";
 import DiscountCardForm from "../pages/forms/discountCard.form";
+import SignupPlayer from "../pages/signup/signupPlayer";
+import SignupCoach from "../pages/signup/signupCoach";
+import SignupArcadeManager from "../pages/signup/signupArcadeManager";
 
 
 const AppRoutes = () => {
@@ -19,10 +21,13 @@ const AppRoutes = () => {
                 <Route path="arcades" element={<Arcades/>}/>
                 <Route path="about" element={<About/>}/>
                 <Route path="login" element={<Login/>}/>
-                <Route path="signup" element={<Signup/>}/>
                 <Route path="bookings" element={<BookingForm/>}/>
                 <Route path="profile/:id" element={<Profiles/>}/>
                 <Route path="forms/discountcardform" element={<DiscountCardForm/>}/>
+                <Route path="signupPlayer" element={<SignupPlayer/>}/>
+                <Route path="signupCoach" element={<SignupCoach/>}/>
+                <Route path="signupArcadeManager" element={<SignupArcadeManager/>}/>
+
 
             </Routes>
         </>
