1c37016a5277e58d0fadb98896dcf599e7cba63a
Wijesinghe W.N.U <141928283+Niwee990519@users.noreply.github.com>
2024-02-16 16:30:51 +0530

    stop for inline css

---
+++ src/components/CoachPageCoachCard.tsx
@@ -0,0 +1,104 @@
+import { Col, Row, Button, Flex } from "antd";
+import "./CoachCard.css";
+import { StarOutlined, StarFilled, StarTwoTone } from "@ant-design/icons";
+import { getTwoToneColor, setTwoToneColor } from "@ant-design/icons";
+import { useState } from "react";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from "@cloudinary/react";
+
+const CoachPageCoachCard = (props: any) => {
+  setTwoToneColor("blue");
+  getTwoToneColor();
+  const [cloudName] = useState("dle0txcgt");
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName,
+    },
+  });
+  return (
+    <>
+      <div className="mainCard">
+        <div className="mainCardsec1"></div>
+        <div className="mainCardsec2">
+          <div className="nameDiscription">
+            <div style={{ marginTop: "8px" }}>
+              <p>Sandun Malage</p>
+              <p className="coachPosition">level one Rugby Coach</p>
+            </div>
+
+            <div className="ratings">
+              <StarFilled style={{ color: "#1B5DB7" }} />
+
+              <StarTwoTone
+                twoToneColor="#1B5DB7"
+                style={{ marginLeft: "10px" }}
+              />
+              <StarTwoTone
+                twoToneColor="#1B5DB7"
+                style={{ marginLeft: "10px" }}
+              />
+              <StarTwoTone
+                twoToneColor="#1B5DB7"
+                style={{ marginLeft: "10px" }}
+              />
+              <StarTwoTone
+                twoToneColor="#1B5DB7"
+                style={{ marginLeft: "10px" }}
+              />
+            </div>
+            <div>
+              <p className="coachDiscription"> {props.description} </p>
+            </div>
+          </div>
+          <div
+            className="priceBooking"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              width: "100%",
+              margin: "0",
+            }}
+          >
+            <div className="price">
+              <p
+                style={{
+                  color: "#5587CC",
+                  fontSize: "16px",
+                  fontWeight: "500",
+                }}
+              >
+                {props.rate}
+              </p>
+              <p style={{ fontWeight: "275", fontSize: "16px" }}>per hour</p>
+            </div>
+            <div className="buttonfeild">
+              <Button
+                type="primary"
+                size="small"
+                style={{
+                  fontSize: "10px",
+                  background: "#5587CC",
+                  fontWeight: "400",
+                }}
+              >
+                Book Coach
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="coachpicture">
+          {" "}
+          <AdvancedImage
+            cldImg={
+              cld.image(props.discount_image)
+              // .resize(Resize.crop().width(200).height(200).gravity('auto'))
+              // .resize(Resize.scale().width(200).height(200))
+            }
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CoachPageCoachCard;
--- src/pages/coaches/coachCardSection.tsx
+++ src/pages/coaches/coachCardSection.tsx
@@ -1,9 +1,30 @@
 import { Col, Row } from "antd";
+import CoachPageCoachCard from "../../components/CoachPageCoachCard";
+import footer.tsx from "../../components/footer";
+
 const CoachCardSection = () => {
   return (
-    <Row>
-      <h1>CoachCardSection</h1>
+    <Row 
+     
+    >
+      <Row  style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        backgroundColor:"#EFF4FA"
+      }}><Col><CoachPageCoachCard/></Col>
+      </Row>
+      
+      
+      <Row>
+        <footer/>
+      </Row>
+     
     </Row>
+
+
+
+
   );
 };
 
