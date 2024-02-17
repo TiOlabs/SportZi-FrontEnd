b5140f6dfb75165e5606e5eb466a271f76124a32
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-02-18 00:21:30 +0530

    bug/ruchith

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -10,35 +10,30 @@ import DiscountCardForm from "../pages/forms/discountCard.form";
 import SignupPlayer from "../pages/signup/signupPlayer";
 import SignupCoach from "../pages/signup/signupCoach";
 import SignupArcadeManager from "../pages/signup/signupArcadeManager";
-
 import CoachProfile from "../pages/profiles/coachProfile";
-
 import PlayerProfileUser from "../pages/profiles/PlayerProfileUsers";
 import CoachAssignDetailsForm from "../pages/forms/coachAssignDetails.form";
-
-
 import CoachProfileUser from "../pages/profiles/CoachProfileUser";
 
-
 const AppRoutes = () => {
-    return (
-        <>
-            <Routes>
-                <Route index element={<Home />}/>
-                <Route path="coaches" element={<Coaches/>} />
-                <Route path="arcades" element={<Arcades/>}/>
-                <Route path="about" element={<About/>}/>
-                <Route path="login" element={<Login/>}/>
-                <Route path="bookings" element={<BookingForm/>}/>
-                <Route path="profile/:id" element={<Profiles/>}/>
-                <Route path="forms/discountcardform" element={<DiscountCardForm/>}/>
-                <Route path="signupPlayer" element={<SignupPlayer/>}/>
-                <Route path="signupCoach" element={<SignupCoach/>}/>
-                <Route path="signupArcadeManager" element={<SignupArcadeManager/>}/>
-
-
+  return (
+    <>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="coaches" element={<Coaches />} />
+        <Route path="arcades" element={<Arcades />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+        <Route path="bookings" element={<BookingForm />} />
+        <Route path="profile/:id" element={<Profiles />} />
+        <Route path="forms/discountcardform" element={<DiscountCardForm />} />
+        <Route path="signupPlayer" element={<SignupPlayer />} />
+        <Route path="signupCoach" element={<SignupCoach />} />
+        <Route path="coacheProfile" element={<CoachProfile />} />
+        <Route path="signupArcadeManager" element={<SignupArcadeManager />} />
+        <Route path="PlayerUSer" element={<PlayerProfileUser />} />
+        <Route path="forms/coachassigndetailsform" element={<CoachAssignDetailsForm />}/>
         <Route path="CoachUser" element={<CoachProfileUser />} />
-
       </Routes>
     </>
   );
