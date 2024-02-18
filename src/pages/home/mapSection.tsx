d0d9dd90e0926a1c3a78e138513e390dc12d8664
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-02-18 13:41:18 +0530

    map

--- src/pages/home/mapSection.tsx
+++ src/pages/home/mapSection.tsx
@@ -1,7 +1,14 @@
 import { Col, Row } from 'antd';
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+
+// use default algorithm and renderer
+
 const MapSction = () => {
+    
+    // const markerCluster = new MarkerClusterer({ map, markers });
     return ( 
         <Row>
+            
             <h1>MapSection</h1>
         </Row>
      );
