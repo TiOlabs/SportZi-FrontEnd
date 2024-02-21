d6d9113c5a03023f1849bf91b1d17f8a2a174ad8
Thisara-Senarathna <thisarasenarathna0824@gmail.com>
2024-02-21 10:53:08 +0530

    setup login2

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -4,20 +4,53 @@ import { Form, Input, Row, Col, Button, Checkbox } from "antd";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import img1 from "./images/img1.png";
 import { off } from "process";
+import axios, { AxiosResponse } from "axios";
 
 const commonInputStyle = {
   height: "40px",
 };
 
 const Login = () => {
+  //   const onFinish = async (values: { username: string; password: string }) => {
+  //     try {
+  //       // Send login request to backend
+  //       const response: AxiosResponse<{ token: string }> = await axios.post(
+  //         "https://localhost:8000/api/login",
+  //         values,
+  //         {
+  //           // Make sure to send over HTTPS
+  //           headers: {
+  //             "Content-Type": "application/json",
+  //           },
+  //         }
+  //       );
+
+  //       // Store token in local storage
+  //       localStorage.setItem("token", response.data.token);
+  //       alert("login successful!");
+
+  //       // Redirect or perform any necessary action upon successful login
+  //       // Example: Redirect to dashboard
+  //       // history.push("/dashboard");
+  //     } catch (error) {
+  //       console.error("Login error:", error);
+  //       alert("login Error!");
+  //       // Handle login error
+  //     }
+  //   };
+
+  
+
+  
+
   return (
     <>
-      <Row >
+      <Row>
         <Col
-          xl={{ span: 12 ,offset:0}}
-          lg={{ span: 12,offset:0}}
-          md={{ span: 20, offset:2}}
-          sm={{ span: 22, offset:1 }}
+          xl={{ span: 12, offset: 0 }}
+          lg={{ span: 12, offset: 0 }}
+          md={{ span: 20, offset: 2 }}
+          sm={{ span: 22, offset: 1 }}
           style={{
             backgroundColor: "#EFF4FA",
             display: "flex",
@@ -26,7 +59,7 @@ const Login = () => {
           }}
         >
           {/* image */}
-          <div style={{ textAlign: "center",paddingBottom:"50px"}}>
+          <div style={{ textAlign: "center", paddingBottom: "50px" }}>
             <img
               src={img1}
               alt=""
@@ -46,9 +79,10 @@ const Login = () => {
             </div>
 
             <div
-            style={{
-                fontSize:"18px"
-            }}>
+              style={{
+                fontSize: "18px",
+              }}
+            >
               <a href=""> Privacy And Policy </a>
             </div>
           </div>
@@ -56,10 +90,10 @@ const Login = () => {
 
         {/* form */}
         <Col
-          xl={{ span: 12, offset:0}}
-          lg={{ span: 12 ,offset:0}}
-          md={{ span: 20, offset:2}}
-          sm={{ span: 22, offset:1}}
+          xl={{ span: 12, offset: 0 }}
+          lg={{ span: 12, offset: 0 }}
+          md={{ span: 20, offset: 2 }}
+          sm={{ span: 22, offset: 1 }}
           style={{ padding: "80px" }}
         >
           <Form
@@ -67,7 +101,6 @@ const Login = () => {
             className="login-form"
             initialValues={{ remember: true }}
             layout="vertical"
-            
             // onFinish={onFinish}
           >
             <div
@@ -75,7 +108,7 @@ const Login = () => {
                 fontSize: "28px",
                 textAlign: "center",
                 paddingBottom: "20px",
-                color:"#0E458E"
+                color: "#0E458E",
               }}
             >
               Unlock Your True Capabilities By Signing In To Access Our
@@ -108,7 +141,7 @@ const Login = () => {
                 placeholder="Password"
               />
             </Form.Item>
-            <Form.Item style={{textAlign:"right"}}>
+            <Form.Item style={{ textAlign: "right" }}>
               {/* <Form.Item name="remember" valuePropName="checked" noStyle>
                 <Checkbox>Remember me</Checkbox>
               </Form.Item> */}
@@ -125,10 +158,10 @@ const Login = () => {
                 style={{
                   height: "40px",
                   width: "100%",
-                  borderRadius:"0",
+                  borderRadius: "0",
                   backgroundColor: "#2E5488",
                   color: "#fff",
-                  fontSize:"16px"
+                  fontSize: "16px",
                 }}
               >
                 Log in
