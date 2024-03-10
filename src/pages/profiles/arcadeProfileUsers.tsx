89d0993efb5d9e1f46d4134c4bb85dd1c56a4b1f
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-03-10 17:04:58 +0530

    32

---
+++ src/pages/profiles/arcadeProfileUsers.tsx
@@ -0,0 +1,12 @@
+import { Col, Row } from "antd";
+import NavbarProfile from "../../components/NavBarProfile";
+const ArcadeProfile = () => {
+  return (
+    <Row>
+      <NavbarProfile />
+      <h1>ArcadeProfile</h1>
+    </Row>
+  );
+};
+
+export default ArcadeProfile;
