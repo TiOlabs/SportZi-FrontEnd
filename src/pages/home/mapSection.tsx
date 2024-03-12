9965ba98738088aaa1ffb3ef449c624fb9aea14c
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-03-12 15:48:01 +0530

    little improvement in map sec

--- src/pages/home/mapSection.tsx
+++ src/pages/home/mapSection.tsx
@@ -1,14 +1,24 @@
 import { Col, Row, Skeleton } from "antd";
-import { useJsApiLoader, GoogleMap, Marker,} from "@react-google-maps/api";
+import { useJsApiLoader, GoogleMap,  } from "@react-google-maps/api";
 import useBreakpoint from "antd/lib/grid/hooks/useBreakpoint";
-import{APIProvider, AdvancedMarker, InfoWindow, Map, Pin} from "@vis.gl/react-google-maps";
-import { useState } from "react";
-const center = { lat: 6.7969, lng: 79.9018 };
-const katubedda ={lat: 	6.801457, lng: 79.899679};
-const piliyandala ={lat: 6.801803, lng: 79.922684};
+import {Marker, MarkerClusterer} from "@googlemaps/markerclusterer";
+import  trees  from "../../data/trees";
+import {
+  APIProvider,
+  AdvancedMarker,
+  InfoWindow,
+  Map,
+  Pin,
+  useMap,
+} from "@vis.gl/react-google-maps";
+import { useEffect, useRef, useState } from "react";
+
 const MapSection = () => {
+  const center = { lat: 43.64, lng: -79.41 };
+  const katubedda = { lat: 6.801457, lng: 79.899679 };
+  const piliyandala = { lat: 6.801803, lng: 79.922684 };
   const { lg } = useBreakpoint();
-const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   return (
     <Row style={{ backgroundColor: "#EFF4FA", marginTop: "2%" }}>
       <Col xs={24} md={10}>
@@ -43,28 +53,142 @@ const [open, setOpen] = useState(false);
       </Col>
       <Col md={1}></Col>
       <Col xs={24} md={13} style={{ padding: "2%" }}>
-        <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY|| ""}>
         <div style={{ height: "50vh", width: "100%" }}>
-        <Map
-          center={center}
-          zoom={15}
-          mapId={process.env.REACT_APP_MAP_ID || ""}
-        >
-          {" "}
-          {/* <Marker position={center} /> */}
-          <AdvancedMarker position={center}></AdvancedMarker>
-          <AdvancedMarker position={katubedda} onClick={()=>setOpen(true)}>
-            <Pin background={"grey"} borderColor={"yellow"} glyphColor={"purple"}></Pin>
-          </AdvancedMarker>
-          <AdvancedMarker position={piliyandala}></AdvancedMarker>
-         
-         {open && <InfoWindow position={katubedda} onCloseClick={()=>setOpen(false)}><p>Im in Katubedda</p></InfoWindow>}
-        </Map>
+          <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ""}>
+            <Map
+              center={{ lat: 43.64, lng: -79.41 }}
+              zoom={10}
+              mapId={process.env.REACT_APP_MAP_ID || ""}
+            >
+              <Markers points={trees}/>
+              {" "}
+              
+              {/* <AdvancedMarker position={center}></AdvancedMarker>
+              <AdvancedMarker
+                position={katubedda}
+                onClick={() => setOpen(true)}
+              >
+                <Pin
+                  background={"grey"}
+                  borderColor={"yellow"}
+                  glyphColor={"purple"}
+                ></Pin>
+              </AdvancedMarker>
+              <AdvancedMarker position={piliyandala}></AdvancedMarker>
+              {open && (
+                <InfoWindow
+                  position={katubedda}
+                  onCloseClick={() => setOpen(false)}
+                >
+                  <p>Im in Katubedda</p>
+                </InfoWindow>
+              )} */}
+            </Map>
+          </APIProvider>
         </div>
-        </APIProvider>
       </Col>
     </Row>
   );
 };
 
 export default MapSection;
+
+
+
+type Point = google.maps.LatLngLiteral & { key: string };
+type Props = { points: Point[] };
+
+const Markers = ({ points }: Props) => {
+  const map = useMap();
+  const [markers, setMarkers] = useState<{ [key: string]: Marker }>({});
+  const clusterer = useRef<MarkerClusterer | null>(null);
+
+  useEffect(() => {
+    if (!map) return;
+    if (!clusterer.current) {
+      clusterer.current = new MarkerClusterer({ map });
+    }
+  }, [map]);
+
+  useEffect(() => {
+    clusterer.current?.clearMarkers();
+    clusterer.current?.addMarkers(Object.values(markers));
+  }, [markers]);
+
+  const setMarkerRef = (marker: Marker | null, key: string) => {
+    if (marker && markers[key]) return;
+    if (!marker && !markers[key]) return;
+
+    setMarkers((prev) => {
+      if (marker) {
+        return { ...prev, [key]: marker };
+      } else {
+        const newMarkers = { ...prev };
+        delete newMarkers[key];
+        return newMarkers;
+      }
+    });
+  };
+
+  return (
+    <>
+      {points.map((point) => (
+        <AdvancedMarker
+          position={point}
+          key={point.key}
+          ref={(marker) => setMarkerRef(marker, point.key)}
+        >
+          <span style={{ fontSize: "2rem" }}>🌳</span>
+        </AdvancedMarker>
+      ))}
+    </>
+  );
+};
+
+
+{/* <GoogleMap
+  center={center}
+  zoom={15}
+  mapContainerStyle={{
+    width: "100%",
+    height: "50vh",
+  }}
+>
+  {" "}
+  <Marker position={center} />
+</GoogleMap>; */}
+
+{
+  /* <Map
+center={center}
+zoom={15}
+mapId={process.env.REACT_APP_MAP_ID || ""}
+>
+{" "} */
+}
+{
+  /* <Marker position={center} /> */
+}
+{
+  /* <AdvancedMarker position={center}></AdvancedMarker>
+<AdvancedMarker
+  position={katubedda}
+  onClick={() => setOpen(true)}
+>
+  <Pin
+    background={"grey"}
+    borderColor={"yellow"}
+    glyphColor={"purple"}
+  ></Pin>
+</AdvancedMarker>
+<AdvancedMarker position={piliyandala}></AdvancedMarker>
+{open && (
+  <InfoWindow
+    position={katubedda}
+    onCloseClick={() => setOpen(false)}
+  >
+    <p>Im in Katubedda</p>
+  </InfoWindow>
+)}
+</Map> */
+}
