0ab7c162536675cc94eb369dd7051b8063e2ba8a
Navindu Chathuranga <125124792+spynav@users.noreply.github.com>
2024-03-12 23:01:57 +0530

    nting

--- src/pages/home/mapSection.tsx
+++ src/pages/home/mapSection.tsx
@@ -1,32 +1,21 @@
 import { Col, Row, Skeleton } from "antd";
-import {
-  GoogleMap,
-  Marker,
-  DirectionsRenderer,
-  Circle,
-  MarkerClusterer,
-} from "@react-google-maps/api";
 import useBreakpoint from "antd/lib/grid/hooks/useBreakpoint";
-import {} from "@googlemaps/markerclusterer";
 import {
   APIProvider,
-  AdvancedMarker,
-  InfoWindow,
   Map,
-  Pin,
   useMap,
+  AdvancedMarker,
 } from "@vis.gl/react-google-maps";
-import { useEffect, useRef, useState } from "react";
-
-const MapSection = () => {
-  // const center = { lat: 6.801457, lng: 79.899679 };
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import type { Marker } from "@googlemaps/markerclusterer";
+import { useEffect, useState, useRef } from "react";
+import trees from "../../data/trees";
+import React from "react";
+export default function MapSection() {
+  const center = { lat: 6.801457, lng: 79.899679 };
   const katubedda = { lat: 6.801457, lng: 79.899679 };
   const piliyandala = { lat: 6.801803, lng: 79.922684 };
   const { lg } = useBreakpoint();
-  const [open, setOpen] = useState(false);
-  const mapOptions = {
-    gestureHandling: "cooperative", // Enable zooming with Ctrl + scroll
-  };
   return (
     <Row style={{ backgroundColor: "#EFF4FA", marginTop: "2%" }}>
       <Col xs={24} md={10}>
@@ -61,12 +50,73 @@ const MapSection = () => {
       </Col>
       <Col md={1}></Col>
       <Col xs={24} lg={13}>
-        <div style={{ height: "50vh", width: "100%" }}></div>
+        <div style={{ height: "50vh", width: "100%" }}>
+          <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY ?? ""}>
+            <MemoizedMap />
+          </APIProvider>
+        </div>
       </Col>
     </Row>
   );
+}
+const MemoizedMap = React.memo(() => (
+  <Map
+    center={{ lat: 43.64, lng: -79.41 }}
+    zoom={10}
+    mapId={process.env.REACT_APP_MAP_ID || ""}
+  >
+    <Markers points={trees} />
+  </Map>
+));
+type Point = google.maps.LatLngLiteral & { key: string };
+type Props = { points: Point[] };
+
+const Markers = ({ points }: Props) => {
+  const map = useMap();
+  const [markers, setMarkers] = useState<{ [key: string]: Marker }>({});
+  const clusterer = useRef<MarkerClusterer | null>(null);
+
+  useEffect(() => {
+    if (!map) return;
+    if (!clusterer.current) {
+      clusterer.current = new MarkerClusterer({ map });
+    }
+  }, [map]);
+
+  useEffect(() => {
+    clusterer.current?.clearMarkers();
+    clusterer.current?.addMarkers(Object.values(markers));
+  }, [markers]);
+
+  const setMarkerRef = (marker: Marker | null, key: string) => {
+    if (marker && markers[key]) return;
+    if (!marker && !markers[key]) return;
+
+    setMarkers((prev) => {
+      if (marker) {
+        return { ...prev, [key]: marker };
+      } else {
+        const newMarkers = { ...prev };
+        delete newMarkers[key];
+        return newMarkers;
+      }
+    });
+  };
+
+  return (
+    <>
+      {points.map((point) => (
+        <AdvancedMarker
+          position={point}
+          key={point.key}
+          ref={(marker) => setMarkerRef(marker, point.key)}
+        >
+          <span style={{ fontSize: "2rem" }}>🌳</span>
+        </AdvancedMarker>
+      ))}
+    </>
+  );
 };
-export default MapSection;
 // const MemoizedMap = React.memo(() => (
 //   <Map
 //     center={{ lat: 43.64, lng: -79.41 }}
