ecab1776aec17314fa75a15f66127b86648461db
Ranasinghe H.B.C.B <141268625+Bhathiya2001@users.noreply.github.com>
2024-03-26 18:41:27 +0530

    NavBarFixedFinal1
    add login signin button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,16 +6,19 @@ import {
 } from "@ant-design/icons";
 import { Divider, Menu } from "antd";
 import { Col, Row } from "antd";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
 import logo2 from "../assets/logoBlack.png";
 import { Popover } from "antd";
 import { Button } from "antd/es/radio";
-import path from "path";
 import Cookies from "js-cookie";
+import { UserContext } from "../context/user.context";
+
 
 const Navbar: React.FC = () => {
+  const { userId } = useContext(UserContext);
+  console.log(userId);
   const [visible, setVisible] = useState(false);
   const [scrolling, setScrolling] = useState(false);
   const { pathname } = useLocation();
@@ -667,7 +670,7 @@ const Navbar: React.FC = () => {
               <Link
                 to="/coaches"
                 style={{
-                  color: pathname === "/coaches" ? "black" : fontColor(),
+                  color: pathname === "/coaches" ? "#01abf8" : fontColor(),
                   textDecoration: "none",
                   position: "relative",
                   display: "inline-block",
@@ -684,7 +687,7 @@ const Navbar: React.FC = () => {
                     bottom: "0",
                     left: coachesUnderlineStyle.left,
                     backgroundColor:
-                      pathname === "/coaches" ? "black" : fontColor(),
+                      pathname === "/coaches" ? "#01abf8" : fontColor(),
                     transition: coachesUnderlineStyle.transition,
                   }}
                 ></span>
@@ -695,7 +698,7 @@ const Navbar: React.FC = () => {
               <Link
                 to="/arcades"
                 style={{
-                  color: pathname === "/arcades" ? "black" : fontColor(),
+                  color: pathname === "/arcades" ? "#01abf8" : fontColor(),
                   textDecoration: "none",
                   position: "relative",
                   display: "inline-block",
@@ -712,7 +715,7 @@ const Navbar: React.FC = () => {
                     bottom: "0",
                     left: arcadeUnderlineStyle.left,
                     backgroundColor:
-                      pathname === "/arcades" ? "black" : fontColor(),
+                      pathname === "/arcades" ? "#01abf8" : fontColor(),
                     transition: arcadeUnderlineStyle.transition,
                   }}
                 ></span>
@@ -722,7 +725,7 @@ const Navbar: React.FC = () => {
               <Link
                 to="/about"
                 style={{
-                  color: pathname === "/about" ? "black" : fontColor(),
+                  color: pathname === "/about" ? "#01abf8" : fontColor(),
                   textDecoration: "none",
                   position: "relative",
                   display: "inline-block",
@@ -739,7 +742,7 @@ const Navbar: React.FC = () => {
                     bottom: "0",
                     left: aboutUnderlineStyle.left,
                     backgroundColor:
-                      pathname === "/about" ? "black" : fontColor(),
+                      pathname === "/about" ? "#01abf8" : fontColor(),
                     transition: aboutUnderlineStyle.transition,
                   }}
                 ></span>
@@ -751,7 +754,7 @@ const Navbar: React.FC = () => {
                 trigger="click"
                 open={open}
                 onOpenChange={handleOpenChange}
-              >
+                >
                 <a className="NavBarUserProfileImgThumsup">
                   <img
                     className="NavBarUserProfileImg"
