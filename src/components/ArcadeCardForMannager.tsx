f5a56b57fdddf2c15696cd38bb6afe2c161a7f0f
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-04-28 22:31:20 +0530

    create arcade cards

--- src/components/ArcadeCardForMannager.tsx
+++ src/components/ArcadeCardForMannager.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { Row, Col } from "antd";
-const ArcadeCardForMannager = () => {
+import { AdvancedImage } from "@cloudinary/react";
+const ArcadeCardForMannager = (props: any) => {
   return (
     <>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap')
+      </style>
       <Row
         style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          alignContent: "center",
           width: "80%",
           height: "60px",
-          border: "1px solid black",
           borderRadius: "3px",
+          transition: "transform 0.2s",
         }}
+        onMouseEnter={(e) => {
+          e.currentTarget.style.transform = "scale(1.05)";
+          e.currentTarget.style.boxShadow = "0px 0px 2px 0px #808080";
+        }} // Scale up on hover and change box shadow
+        onMouseLeave={(e) => {
+          e.currentTarget.style.transform = "scale(1)";
+          e.currentTarget.style.boxShadow = "0px 0px 0px 0px #808080";
+        }} // Reset scale and box shadow when not hovering
       >
         <Col
           xs={6}
@@ -19,17 +36,32 @@ const ArcadeCardForMannager = () => {
             alignItems: "center",
           }}
         >
-          <div style={{ borderRadius: "50%" }}></div>
+          <div
+            style={{
+              borderRadius: "50%",
+              backgroundColor: "red",
+              width: "40px",
+              height: "40px",
+            }}
+          ></div>
         </Col>
         <Col
           xs={18}
           style={{
             display: "flex",
-
             alignItems: "center",
           }}
         >
-          <h1>SSC cricket</h1>
+          <p
+            style={{
+              fontSize: "16px",
+              fontWeight: "500",
+              color: "#000",
+              fontFamily: "kanit",
+            }}
+          >
+            {props.name}
+          </p>
         </Col>
       </Row>
     </>
--- src/pages/login/chooseArcade.tsx
+++ src/pages/login/chooseArcade.tsx
@@ -1,12 +1,29 @@
 import { Col, Row } from "antd";
-import React from "react";
+import React, { useEffect } from "react";
 import ArcadeCardForMannager from "../../components/ArcadeCardForMannager";
+import axiosInstance from "../../axiosInstance";
 
 const ChooseArcade = () => {
+  useEffect(() => {
+    console.log("useeffect");
+    axiosInstance
+      .get("/api/auth/getchoosenArcade/")
+      .then((res) => {
+        console.log("kanishka", res.data);
+      })
+      .catch((err) => {
+        console.log("errorrrrrrrrrr", err);
+      });
+  }, []);
+
   return (
     <>
+      <style>
+        overflowY: "hidden", @import
+        url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap')
+      </style>
       <Row style={{ width: "100%", height: "100vh" }}>
-        <Col xs={8} style={{ backgroundColor: "#000" }}></Col>
+        <Col xs={8}></Col>
         <Col
           xs={8}
           style={{
@@ -17,9 +34,22 @@ const ChooseArcade = () => {
             alignItems: "center",
           }}
         >
-          <ArcadeCardForMannager />
+          <p
+            style={{
+              fontSize: "24px",
+              fontWeight: "500",
+              color: "#000",
+              fontFamily: "kanit",
+              marginBottom: "20px",
+            }}
+          >
+            Choose your Arcade
+          </p>
+          <ArcadeCardForMannager name="ssc Ground" />
+          <ArcadeCardForMannager name="colombo city" />
+          <ArcadeCardForMannager name="magestic city" />
         </Col>
-        <Col xs={8} style={{ backgroundColor: "#000" }}></Col>
+        <Col xs={8}></Col>
       </Row>
     </>
   );
