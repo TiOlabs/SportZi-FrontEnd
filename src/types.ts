6235178a7327e21172d352fdf7572ad4ded139bc
Thisara-Senarathna <thisarasenarathna0824@gmail.com>
2024-06-10 18:39:53 +0530

    add and display feedbacks
    AddFeedbacks, display feedbacks and display AvgFeedbacks in arcade profile

--- src/types.ts
+++ src/types.ts
@@ -261,6 +261,37 @@ export interface CoachFeedback {
   };
 }
 
+export interface ArcadeFeedback {
+  arcade_feedback_id: string;
+  rate: number;
+  arcade_id: string;
+  feedback: {
+    feedbacks_id: string;
+    user_id: string;
+    user: {
+      user_id: string;
+      role: string;
+      firstname: string;
+      lastname: string;
+      email: string;
+      password: string;
+      DOB: string;
+      gender: string;
+      accountNumber: string;
+      is_active: string;
+      user_image: string;
+      address: string;
+      city: string;
+      country: string;
+      description: string;
+    };
+    feedbackComments: {
+      feedback_id: string;
+      comment: string;
+    };
+  };
+}
+
 export interface CoachEnrollDetailsForPackages {
   coach_id: String;
   package_id: String;
